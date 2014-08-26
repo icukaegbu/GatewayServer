75f4a209b9beb7caf69819ee2d0694ef39b6c44e
Gabs00 <gewen87@gmail.com>
2014-08-26 06:50:53 -0400

    Completed implimentation allow servers to register to and receive queries from the Gateway server

--- server-conf.js
+++ server-conf.js
@@ -1,10 +1,42 @@
-var express = require('express');
 var env = require('./helpers/env');
+var io = require('socket.io')();
+var express = require('express');
+var handler = require('./helpers/request-handlers');
+var parser = require('body-parser');
+
 var app = express();
 
-app.use('/', function(req, res){
-  res.send('Hello World');
+io.listen(env.port);
+handler.log('listening on ', env.port);
+io.on('connection', function(socket){
+
+  handler.log('received a connection');
+
+  //requests authentication secret from this socket
+  socket.on('register', function(data){
+    handler.register(data, socket);
+  });
+
+  //Authenticates servers who alread have secrets
+  socket.on('auth', function(data){
+    handler.auth(data, socket, Auth);
+  });
+
+  //Sets event handlers once socket is authorized
+  function Auth(){
+    var store = {};
+    socket.on('info', function(data){
+      store.info = data;
+      handler.info(data, socket);
+    });
+
+    socket.on('disconnect', function(socket){
+      handler.deleteSocket(store.info, socket);
+    });
+  }
 });
 
-module.exports.port = env.port;
-module.exports.app = app;
+app.use(parser.json());
+app.get('/server', handler.redirectTo);
+app.listen(env.port+1);
+
--- server.js
+++ server.js
@@ -1,4 +1,1 @@
 var server = require('./server-conf');
-
-server.app.listen(server.port);
-console.log('Gateway server listening on port', server.port);
