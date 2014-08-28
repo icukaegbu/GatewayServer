66850d03b4066930ed0a04d44db19cfa41609727
icukaegbu <icukaegbu@gmail.com>
2014-08-28 15:16:17 +0100

    Refactored the Spawn process into a method

--- servers/servertest.js
+++ servers/servertest.js
@@ -1,11 +1,49 @@
+var serverHelper = require('../helpers/server-helpers.js');
 var spawn = require('child_process').spawn;
 var servercount = process.argv[2] || 10;
 var portStart = 5000;
 var name = 'Auth_';
 var servers = [];
+
+//REFACTOR
 while(servers.length < servercount){
   var index = servers.length;
   var port = portStart++;
   var n = name + port;
   servers.push(spawn('node', ['./servers.js', port, n], {stdio:'inherit'}));
 }
+for (var i = 0; i < servers.length; i++) {
+	console.log(servers[i]);
+};
+//console.log(servers);
+
+// var initConnections = function(){
+// 	//open a connection to the DB (DONE EXPLICITLY BY SERVERHELPER)
+
+// 	//retrieve the list of all servers DB
+// 	servers = serverHelper.servers();
+
+// 	//iterate thru the db list and spawn new processes
+// }
+
+// //open a connection to the DB
+// var openConnection = function(){
+// 	return serverHelper
+// }
+
+//add a server to the DB
+//	authenticate the server, if he does not exist, add him to the db and to the allowed schema
+
+
+var spawnServer = function(server){
+	servers.push(spawn('node', ['./servers.js', server.port, server], {stdio:'inherit'}));
+}
+
+//retrieve the list of all servers DB
+
+//iterate thru the db list and spawn new processes
+//refactor out the spawn method to iterate thru the list of servers and spawn them
+
+//log the spawn method to the screen
+
+//update the UI to show connected servers
