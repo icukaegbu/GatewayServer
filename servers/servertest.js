966a4567d44378ea73989b23ad6303e11ccc788f
icukaegbu <icukaegbu@gmail.com>
2014-08-28 15:16:36 +0100

    Added the addServer method

--- servers/servertest.js
+++ servers/servertest.js
@@ -34,6 +34,29 @@ for (var i = 0; i < servers.length; i++) {
 //add a server to the DB
 //	authenticate the server, if he does not exist, add him to the db and to the allowed schema
 
+var addServer = function(){
+	//spawn the server
+	var newServer = {};
+	newServer.port = portStart++;
+	newServer.name = name + newServer.port;
+
+	//if port already exists in db, regenerate it
+	while ( serverHelper.find( newServer.name ) ){
+		newServer.port = portStart++;
+		newServer.name = name + newServer.port;
+	}
+
+	//add date of creation
+	newServer.createdOn = new Date.now;
+
+	//if server does not exit, add to db and spawn
+	if ( serverHelper.add(newServer, newServer.port) ) {
+		console.log('Added new server: ' + newServer.name);
+		spawnServer(newServer);
+	}
+
+	return newServer;
+}
 
 var spawnServer = function(server){
 	servers.push(spawn('node', ['./servers.js', server.port, server], {stdio:'inherit'}));
